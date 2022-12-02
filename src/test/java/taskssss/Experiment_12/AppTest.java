b4afefa76200db8571959e19ddbee41e00b26c39
HI <HI@DESKTOP-TDEOMCR>
2022-12-02 09:50:51 -0800

    this ia experiment 12

---
+++ src/test/java/taskssss/Experiment_12/AppTest.java
@@ -0,0 +1,31 @@
+package taskssss.Experiment_12;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	@Test
+	public void test() {
+
+        App p = new App();
+        assertEquals("Hello Bindu", p.getMessage("Bindu"));
+
+    }
+    @Test
+    public void testNameEmpty() {
+
+        App c = new App();
+        assertEquals("Please provide a name!", c.getMessage(" "));
+    }
+
+}
